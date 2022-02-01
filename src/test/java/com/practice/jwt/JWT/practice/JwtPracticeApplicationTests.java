b5a83d19d72cf4bb16295c496d6e4db2592fd88d
Kishor Roy <roykishor93@gmail.com>
2022-02-01 16:21:09 +0600

    initial commit

---
+++ src/test/java/com/practice/jwt/JWT/practice/JwtPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.jwt.JWT.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
